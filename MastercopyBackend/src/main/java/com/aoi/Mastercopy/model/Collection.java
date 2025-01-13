3dbd9071cb7efe0e39aceba56cc50342d002066b
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2025-01-13 15:14:17 -0500

    Initial commit

---
+++ MastercopyBackend/src/main/java/com/aoi/Mastercopy/model/Collection.java
@@ -0,0 +1,40 @@
+package com.aoi.Mastercopy.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collection {
+
+    private String id;
+    private String name;
+    // TODO TIFF: This should be a set or a hashmap or something instead
+    private ArrayList<String> savedArt;
+
+    public Collection() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getSavedArt() {
+        return savedArt;
+    }
+
+    public void setSavedArt(ArrayList<String> savedArt) {
+        this.savedArt = savedArt;
+    }
+}
---
+++ MastercopyBackend/src/test/java/com/aoi/Mastercopy/MastercopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoi.Mastercopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastercopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
