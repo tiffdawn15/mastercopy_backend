38760c6a7c01edccd8f3f34d7ccde3b2f95b2c49
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2025-01-30 15:57:37 -0500

    First api endpoint works.

--- MastercopyBackend/src/main/java/com/aoi/Mastercopy/repository/CollectionRepository.java
+++ MastercopyBackend/src/main/java/com/aoi/Mastercopy/repository/CollectionRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "collection", path = "collection")
 public interface CollectionRepository extends MongoRepository<Collection, String> {
 
-    List<Collection> findByName(@Param("name") String name);
+//    List<Collection> findByName(@Param("name") String name);
 
 }
