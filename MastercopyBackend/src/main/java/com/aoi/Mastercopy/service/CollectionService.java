3310d280d009b8c5b859e6949aca90f2961db85a
Tiffany Messer <tiffanymesser@tiffanys-mbp.mynetworksettings.com>
2025-01-30 15:32:43 -0500

    Updated code

---
+++ MastercopyBackend/src/main/java/com/aoi/Mastercopy/service/CollectionService.java
@@ -0,0 +1,19 @@
+package com.aoi.Mastercopy.service;
+
+import com.aoi.Mastercopy.repository.CollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class CollectionService {
+
+    @Autowired
+    private CollectionRepository collectionRepository;
+
+    public List<Collection> getAllCollections() {
+        return collectionRepository.findAll();
+    }
+}
