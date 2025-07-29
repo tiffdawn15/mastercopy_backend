f6dd3e1482b6698b13d7bec685df6aeca560b345
Tiffany Messer <tiffanymesser15@gmail.com>
2025-07-29 13:31:24 -0400

    Updated for build.

---
+++ src/main/java/com/aoi/Mastercopy/dto/CollectionDTO.java
@@ -0,0 +1,9 @@
+package com.aoi.Mastercopy.dto;
+
+public class CollectionDTO {
+    private String name;
+
+    // getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
--- src/main/java/com/aoi/Mastercopy/service/CollectionService.java
+++ src/main/java/com/aoi/Mastercopy/service/CollectionService.java
@@ -1,10 +1,11 @@
 package com.aoi.Mastercopy.service;
 
+import com.aoi.Mastercopy.dto.CollectionDTO;
+import com.aoi.Mastercopy.model.Collection;
 import com.aoi.Mastercopy.repository.CollectionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -15,5 +16,23 @@ public class CollectionService {
 
     public List<Collection> getAllCollections() {
         return collectionRepository.findAll();
+    }
+
+    public boolean createCollection(CollectionDTO collectionDTO){
+        if (collectionDTO == null || collectionDTO.getName() == null || collectionDTO.getName().isEmpty()) {
+            return false;
+        }
+
+        // TODO TIFF: Update with Builder pattern
+        Collection collection = new Collection();
+        collection.setName(collectionDTO.getName());
+
+        try {
+            collectionRepository.save(collection);
+            return true; // Collection created successfully
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; // Error occurred while creating the collection
+        }
     }
 }
